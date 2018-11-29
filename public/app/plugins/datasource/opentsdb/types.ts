cb98b4ccb11f5fd9ed8c998eaff844820cbdbe37
Gabe E. Nydick <gabe@ridezum.com>
2018-11-28 17:31:43 -0800

    opentsdb-gexp patches

--- public/app/plugins/datasource/opentsdb/types.ts
+++ public/app/plugins/datasource/opentsdb/types.ts
@@ -2,6 +2,8 @@ import { DataQuery, DataSourceJsonData } from '@grafana/data';
 
 export interface OpenTsdbQuery extends DataQuery {
   metric?: any;
+  queryType: string;
+  gexp: string;
 }
 
 export interface OpenTsdbOptions extends DataSourceJsonData {
